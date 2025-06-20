d136ed40510890565a3c9dedad6c0fa48e2460d3
Kokushi Hattori <kokushi@tsugu.io>
2025-06-20 08:51:21 +0000

    fix: add wsgi_app & bind in gunicorn.conf

--- match_api/gunicorn.conf.py
+++ match_api/gunicorn.conf.py
@@ -1,2 +1,6 @@
 # gunicorn.conf.py
-workers = 1
+workers   = 1                 # 1 vCPU なので固定 1
+wsgi_app  = "main:app"        # ← これ必須!
+bind      = "0.0.0.0:8080"    # Cloud Run デフォルトポート
+timeout   = 120               # (任意) 60→120 秒に延長
+# preload_app = True          # (任意) OOM 出なければ有効に

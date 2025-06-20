818d671959d16f4276ce5237a9251b9e925dfeb2
Kokushi Hattori <kokushi@tsugu.io>
2025-06-20 08:57:16 +0000

    fix: point wsgi_app to match_api.main

--- match_api/gunicorn.conf.py
+++ match_api/gunicorn.conf.py
@@ -1,6 +1,6 @@
 # gunicorn.conf.py
 workers   = 1                 # 1 vCPU なので固定 1
-wsgi_app  = "main:app"        # ← これ必須!
+wsgi_app  = "match_api.main:app"   # ← パッケージ名を付ける
 bind      = "0.0.0.0:8080"    # Cloud Run デフォルトポート
 timeout   = 120               # (任意) 60→120 秒に延長
 # preload_app = True          # (任意) OOM 出なければ有効に
